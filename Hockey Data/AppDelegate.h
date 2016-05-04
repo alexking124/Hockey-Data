b51d830cb1fac5649e4a00cfc89a5af93dd71418
Alex King <alexking@alexs-macbook-pro.local>
2016-05-04 11:55:34 -0700

    Initial Commit

---
+++ Hockey Data/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hockey Data
+//
+//  Created by Alex King on 5/4/16.
+//  Copyright © 2016 Alex King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

0d63598c3b508cb49152fc55899c2c1b2d0b6b41
Alex King <alexking124@gmail.com>
2017-10-15 16:39:06 -0700

    Added Tracker file

---
+++ Hockey Data/Tracker.swift
@@ -0,0 +1,13 @@
+//
+//  Tracker.swift
+//  HockeyData
+//
+//  Created by Alex King on 10/1/17.
+//  Copyright © 2017 Alex King. All rights reserved.
+//
+
+import Firebase
+
+struct Tracker {
+    
+}
